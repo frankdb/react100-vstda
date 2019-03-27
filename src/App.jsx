99849cc486416baa9daf1cb0ed13c63ce73ecb40
frankdb <fd.barros@yahoo.com>
2019-03-27 11:01:49 -0700

    Added new components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className='container'>
+        <h1>Very Simple Todo App</h1>
+        <h4>Track all of the things</h4>
+
+        <div className="row">
+          <div className="col-md-4">
+            Hello
+          </div>
+
+          <div className="col-md-8">
+            World
+          </div>
+
+
+        </div>
 
       </div>
     );
